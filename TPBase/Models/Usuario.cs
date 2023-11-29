a8a244bda99c899b2c69ffc7bbcca71ab985fc98
Agus Potasman <128632108+AgustinaPotasman@users.noreply.github.com>
2023-11-29 09:32:27 -0300

    Update

--- TPBase/Models/Usuario.cs
+++ TPBase/Models/Usuario.cs
@@ -3,9 +3,9 @@ namespace TPBase.Models
 {
     public class Usuario
     {
-        public int IdUsuario { get; set; }
-        public string Nombre { get; set; }
-        public string Contraseña { get; set; }
+        public int IdUsuario {get; set;}
+        public string Nombre {get; set;}
+        public string Contraseña {get; set;}
 
         public Usuario(int idUsuario, string contraseña, string nombre)
         {
