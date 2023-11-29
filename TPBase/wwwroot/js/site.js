57984f7bf56dd8b079eae9c50f75fef430fb068e
Agus Potasman <128632108+AgustinaPotasman@users.noreply.github.com>
2023-11-29 11:24:01 -0300

    Supuestamente terminado js (a chequear)

--- TPBase/wwwroot/js/site.js
+++ TPBase/wwwroot/js/site.js
@@ -11,3 +11,18 @@ function CrearCuenta() {
 
     })
 }
+
+
+function MostrarMasInfo(idC) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: '/Home/MostrarMasInfoAjax',
+        data: { Idconcierto: idC },
+        success: function (response) {
+            console.log(response);
+            $("#FechaConcierto").html("Fecha de lanzamiento: " + response.fechaConcierto.substr(0, response.fechaConcierto.length - 10)).addClass("custom-info");
+            $("#Descripcion").html(response.descripcion).addClass("custom-info");
+        }
+    });
+}
