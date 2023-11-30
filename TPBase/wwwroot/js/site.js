52013aca7f4f74e93250f7629b22d73f872f7cf4
AGUSTINA POTASMAN <47572491@ort>
2023-11-30 13:40:01 -0300

    actualizado

--- TPBase/wwwroot/js/site.js
+++ TPBase/wwwroot/js/site.js
@@ -3,7 +3,7 @@ function CrearCuenta() {
         type: 'POST',
         dataType: 'JSON',
         url: '/Home/CrearCuentaAjax',
-        //data: { IdUsuario: idU },
+        data: { IdUsuario: idU },
         success: function (response) {
             let content = "Nombre de usuario: <input type='text' class='play' id='player' name='Nombre' placeholder='Ingrese su nombre'></input>"
             $("#CrearCuenta").html(content);
